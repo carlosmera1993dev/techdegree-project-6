468dc3938d1040091efe2a97dd5665b5670f2070
carlosmera1993dev <59983550+carlosmera1993dev@users.noreply.github.com>
2020-10-17 00:18:03 -0500

    finished gaem logic, lacks polishing

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
-const qwerty = document.getElementById('qwerty');
+const gameKeyboard = document.getElementById('qwerty');
 const gamePhrase = document.getElementById('phrase');
 
+//Variable for reseting the game 
+let gameStatus = false;
+
 //Variable to keep track of wrong guesses
-const missed = 0;
+let missed = 0;
 
 //Start game button 
-const start = document.querySelector('.btn__reset');
+const startButton = document.querySelector('.btn__reset');
+const overlay = startButton.parentNode;
 
 //Event listener to start game
-start.addEventListener('click', () => {
-
+startButton.addEventListener('click', () => {  
+    overlay.style.display = 'none';
 });
+
+//Function to check if game is won or lost
+function checkWin() {
+    let classLetter = document.querySelectorAll('.letter');
+    let classShow = document.querySelectorAll('.show');
+    let title = overlay.firstElementChild;
+    if (classLetter.length === classShow.length) {
+        overlay.className = 'win';
+        overlay.style.display = '';
+        title.textContent = 'You win!'
+        startButton.textContent = 'Play again';
+        gameStatus = true;
+        resetGame();
+    } else if (missed === 5) {
+        overlay.className = 'lose';
+        overlay.style.display = '';
+        title.textContent = "You lost!";
+        startButton.textContent = 'Play again';
+        gameStatus = true;
+        resetGame();
+    }
+}
 
 //Game phrases array
 const phrases = ['Dormammu Ive come to bargain', 'I am Groot', 'Hulk smash', 'I am Iron Man', 'I can do this all day'];
@@ -33,7 +59,7 @@ function getRandomPhraseAsArray(arr) {
 }
 
 //create a random phrase
-const phraseArray = getRandomPhraseAsArray(phrases);
+let phraseArray = getRandomPhraseAsArray(phrases);
 
 //Function to loop through the current phrase and store each character 
 //in a 'li', and then append it to the 'gamePhrase' ul
@@ -50,20 +76,73 @@ function addPhraseToDisplay(arr) {
             li.className = 'letter'
         }
         //append li to the ul
-        gamePhrase.appendChild(li);
+        // gamePhrase.appendChild(li);
+        gamePhrase.firstElementChild.appendChild(li);
     }
 }
+
+addPhraseToDisplay(phraseArray);
 
 //Function to check if letter is in the phrase 
 function checkLetter(button) {
     const phraseLetters = document.querySelectorAll('.letter');
+    let rightOrWrong = false;
+    let correctGuess = button;
     for (let i = 0; i < phraseLetters.length; i++) {
-        if (button === phraseLetters[i]) {
-            phraseLetters[i].className = 'show';
-            let correctGuess = button;
-            return correctGuess
-        } else {
-            return null
+        let currentLetter = phraseLetters[i];
+        if (button == currentLetter.textContent.toLowerCase()) {
+            currentLetter.className += ' show';
+            rightOrWrong = true;
         }
     }
+    if (rightOrWrong === true) {
+        return correctGuess
+    }  else {
+        return null
+    }
 }
+
+//function for removing tries
+function removeTries() {
+    let scoreboard = document.getElementById('scoreboard').firstElementChild;
+    let heart = scoreboard.children;
+    scoreboard.removeChild(heart[0]);
+}
+
+//Listening for clicks and handling letters
+gameKeyboard.addEventListener('click', (event) => {
+    const key = event.target;
+    key.className = 'chosen';
+    key.disabled = true;
+    let letterFound = checkLetter(key.textContent);
+    if (letterFound === null) {
+        missed += 1;
+        removeTries();
+    }
+    checkWin();
+})
+
+//Function for reseting the game 
+function resetGame() {
+    //reset game 
+    gameStatus = false;
+    //reset keyboard
+    let usedKeys = document.querySelectorAll('.chosen');
+    for (let i = 0; i < usedKeys.length; i++) {
+        usedKeys[i].className = '';
+        usedKeys[i].disabled = false;
+    }
+    //Reset tries
+    missed = 0;
+    //add hearts until there are five 
+
+
+
+    //clear old phrase 
+    gamePhrase.firstElementChild.innerHTML = '';
+    //generate new random phrase
+    phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
+}
+
+//Fix hightlighting of rows!!!!!!!
